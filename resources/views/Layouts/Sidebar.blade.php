3df999a4a7a8fa70f3b230bd992b26a381dc2f83
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2026-02-03 22:27:11 +0800

    done

--- resources/views/Layouts/Sidebar.blade.php
+++ resources/views/Layouts/Sidebar.blade.php
@@ -1,37 +1,71 @@
 <!-- Menu -->
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+
+    <!-- Brand -->
     <div class="app-brand demo">
         <a href="/" class="app-brand-link">
             <span class="app-brand-logo demo">
-                <img src="{{ asset('assets/img/thrift.png') }}" alt="Logo" class="img-fluid" width="50"
-                    height="50">
+                <img src="{{ asset('assets/img/thrift.png') }}" alt="Logo"
+                     class="img-fluid" width="50" height="50">
             </span>
             <span class="text-start app-brand-text fw-bold ms-2">
                 <small>Filtering</small><br>
-                <small>thrift</small><br>
-                <small></small>
+                <small>Thrift</small>
             </span>
         </a>
 
-        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+        <a href="javascript:void(0);"
+           class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
             <i class="bx bx-chevron-left bx-sm align-middle"></i>
         </a>
     </div>
+    <!-- /Brand -->
 
     <div class="menu-inner-shadow"></div>
 
     <ul class="menu-inner py-1">
-        <!-- ==================== PENGATURAN AKUN ==================== -->
+
+        <!-- ==================== UTAMA ==================== -->
         <li class="menu-header small text-uppercase">
             <span class="menu-header-text">Utama</span>
         </li>
 
-        <!-- ==================== DATA MASTER ==================== -->
         <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
             <a href="/dashboard" class="menu-link">
-                <i class="menu-icon fa-solid fa-user-graduate"></i>
+                <i class="menu-icon fa-solid fa-chart-line"></i>
                 <div>Dashboard</div>
             </a>
+        </li>
+
+        <!-- ==================== DATA MASTER ==================== -->
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Data Master</span>
+        </li>
+
+        <li class="menu-item {{ request()->is('toko') ? 'active' : '' }}">
+            <a href="/toko" class="menu-link">
+                <i class="menu-icon fa-solid fa-store"></i>
+                <div>Toko</div>
+            </a>
+        </li>
+
+        <li class="menu-item {{ request()->is('kategori') ? 'active' : '' }}">
+            <a href="/kategori" class="menu-link">
+                <i class="menu-icon fa-solid fa-tags"></i>
+                <div>Kategori</div>
+            </a>
+        </li>
+
+        <li class="menu-item {{ request()->is('produk-admin') ? 'active' : '' }}">
+            <a href="/produk-admin" class="menu-link">
+                <i class="menu-icon fa-solid fa-box-open"></i>
+                <div>Produk</div>
+            </a>
+        </li>
+
+        <!-- ==================== MANAJEMEN USER ==================== -->
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Manajemen User</span>
         </li>
 
         <li class="menu-item {{ request()->is('user') ? 'active' : '' }}">
@@ -41,25 +75,12 @@
             </a>
         </li>
 
-        <li class="menu-item {{ request()->is('toko') ? 'active' : '' }}">
-            <a href="/toko" class="menu-link">
-                <i class="menu-icon fa-solid fa-store"></i>
-                <div>Toko</div>
+        <li class="menu-item {{ request()->is('setting-user') ? 'active' : '' }}">
+            <a href="/setting-user" class="menu-link">
+                <i class="menu-icon fa-solid fa-gear"></i>
+                <div>Role & Akses</div>
             </a>
         </li>
-        <li class="menu-item {{ request()->is('kategori') ? 'active' : '' }}">
-            <a href="/kategori" class="menu-link">
-                <i class="menu-icon fa-solid fa-award"></i>
-                <div>Kategori</div>
-            </a>
-        </li>
-        <li class="menu-item {{ request()->is('produk-admin') ? 'active' : '' }}">
-            <a href="/produk-admin" class="menu-link">
-                <i class="menu-icon fa-solid fa-box-open"></i>
-                <div>Produk</div>
-            </a>
-        </li>
-
 
     </ul>
 </aside>
