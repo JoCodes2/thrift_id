de9a505cb9a835ede68c1160decf4cc64e5c0d1c
divta-suryawan <gitdivta@gmail.com>
2026-02-08 15:18:09 +0800

    fixing app

--- resources/views/ui/base.blade.php
+++ resources/views/ui/base.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ThriftVibe - Sustainable Fashion</title>
+     <link rel="icon" type="image/png" href="{{ asset('assets/img/thrift.png') }}" />
       <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
